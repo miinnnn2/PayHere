9d6e7d958297d5a1d9ea92b729ae8e7b953c1c93
miinnnn2 <44335785+miinnnn2@users.noreply.github.com>
2022-09-29 23:52:03 +0900

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -19,7 +19,7 @@ export const typedFetch = async <T>(params: FetchParams) => {
   const res = await fetch(reqURL, {
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
-      Authorization: `Bearer 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`,
+      Authorization: `Bearer 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`,
       Accept: 'application/vnd.github.text-match+json'
     },
     mode: 'cors',
